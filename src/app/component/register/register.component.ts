01ec081dae33aad72a3fd2d6554d1705bc481be8
Henry Aimre <henryaimre@gmail.com>
2019-05-08 13:39:34 +0300

    removed forms extra fields

--- src/app/component/register/register.component.ts
+++ src/app/component/register/register.component.ts
@@ -11,12 +11,9 @@ import { MustMatch } from './must-match.validator';
 })
 export class RegisterComponent implements OnInit {
  users = ['Driver', 'Customer'];
- signupForm: FormGroup;
- signupFormDriver: FormGroup;
 
- carsList = ['Alfa Romeo', 'Aston Martin', 'BMW', 'Bentley', 'Cadillac', 'Chevrolet'];
- modelList = ['147', '159', '3 series', '5 series', 'Aveo', 'Escalade'];
- listOfColors = ['white', 'pink', 'red', 'orange', 'yellow', 'green', 'blue', 'purple', 'brown', 'black', 'gray'];
+
+
 
 
  submitted = false;
@@ -48,35 +45,8 @@ export class RegisterComponent implements OnInit {
     {
        MustMatch('password', 'password2');
       }
-
-    this.signupFormDriver = new FormGroup({
-      // tslint:disable-next-line:object-literal-key-quotes
-      'car': new FormControl('Alfa Romeo', Validators.required),
-      // tslint:disable-next-line:object-literal-key-quotes
-      'model': new FormControl('147', Validators.required),
-      // tslint:disable-next-line:object-literal-key-quotes
-      'color': new FormControl('white', Validators.required),
-      // tslint:disable-next-line:object-literal-key-quotes
-      'selectfile': new FormControl('white', Validators.required),
-      // tslint:disable-next-line:object-literal-key-quotes
-      'licence': new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(12)]),
-      // tslint:disable-next-line:object-literal-key-quotes
-      'plate': new FormControl('', [Validators.required, Validators.minLength(5), Validators.maxLength(7)] )
-    },
-);
   }
-    userroleSelector(control: FormControl): Promise<any> | Observable <any> {
-      const promise = new Promise<any>((resolve, reject) => {
-        setTimeout(() => {
-          if (control.value === 'Customer') {
-            resolve({ showDriverfrom: true });
-          } else {
-            resolve(null);
-          }
-        }, 1);
-      });
-      return promise;
-    }
+
    // convenience getter for easy access to form fields
    get f() { return this.signupForm.controls; }
   onSubmit() {
